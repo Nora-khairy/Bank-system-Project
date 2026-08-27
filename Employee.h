ab11d052ae2652e1763b975662b3b81ccb70bdc4
Nada Abdelrazek <nadaabdelrazel@gmail.com>
2026-08-27 06:03:02 +0300

    Add files via upload

---
+++ Employee.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Validation.h"
+#include "Person.h"
+class Employee :public Person
+{
+private:
+	double salary;
+public:
+	//Constructor
+	Employee() {}
+
+
+	Employee(int Id, string Name, string Pasword, double salary) : Person( Id,Name, Pasword) {
+		setSalary(salary);
+	}
+	//setter salary 
+	void setSalary(double salary) {
+		if (Validation::isValidSalary(salary)) {
+			this->salary = salary;
+		}
+		else {
+			cout << "Invalid Salary" << endl;
+		}
+	}
+
+
+	//GETTER salary
+	double  getSalary() {
+		return this->salary;
+	}
+	//display 
+	void display() {
+		displayPerson();
+		cout << "Salary:" << salary << endl;
+	}
+
+};
