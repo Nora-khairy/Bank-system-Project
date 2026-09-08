0fb5a059d21e2d60a764cff4b04e069493df52bf
Nora-khairy <khairyn714@gmail.com>
2026-09-08 08:07:05 -0700

    Update Validation.h

--- Validation.h
+++ Validation.h
@@ -7,44 +7,82 @@
 
 using namespace std;
 
-class Validation {
+class Validation
+{
 public:
-    static bool isValidName(const string& name) {
-        if (name.length() < 3 || name.length() > 20) return false;
-        for (char c : name) {
-            if (!isalpha(c) && c != ' ') return false;
+
+    static bool isValidName(const string& name)
+    {
+        if (name.length() < 5 || name.length() > 20)
+            return false;
+
+        for (char c : name)
+        {
+            if (!isalpha(c) && c != ' ')
+                return false;
         }
+
         return true;
     }
 
-    static bool containsSpecialChar(const string& password) {
-        for (char c : password) {
-            if (!isalnum(c)) return true;
+    static bool containsSpecialChar(const string& password)
+    {
+        for (char c : password)
+        {
+            if (!isalnum(c))
+                return true;
         }
+
         return false;
     }
 
-    static bool isValidPassword(const string& password) {
-        if (password.length() < 8 || password.length() > 20) return false;
+    static bool isValidPassword(const string& password)
+    {
+        if (password.length() < 8 || password.length() > 20)
+            return false;
+
+        for (char c : password)
+        {
+            if (isspace(c))
+                return false;
+        }
+
         return containsSpecialChar(password);
     }
 
-    static bool isValidBalance(double balance) {
+ 
+    static bool isValidBalance(double balance)
+    {
         return balance >= 1500.0;
     }
 
-    static bool isValidSalary(double salary) {
+    static bool isValidSalary(double salary)
+    {
         return salary >= 5000.0;
     }
-
-    static string enterName() {
-        string name;
-        cout << "Enter Name: ";
-        getline(cin >> ws, name);
+    static bool manageName(string& name) {
         while (!isValidName(name)) {
             cout << "Invalid Name! Please enter again: ";
             getline(cin >> ws, name);
         }
+
+        return true;
+    }
+
+    
+    static string enterName()
+    {
+        string name;
+
+        cout << "Enter Name: ";
+        getline(cin >> ws, name);
+
+        while (!isValidName(name))
+        {
+            cout << "Invalid Name! Please enter again: ";
+            getline(cin >> ws, name);
+        }
+
         return name;
     }
 
@@ -58,6 +96,7 @@ public:
         }
         return password;
     }
+
 };
 
-#endif
+#endif ده كود بتاعك الصح ممكن تبصي فيه عدل فيه شويه حاجات بساط
