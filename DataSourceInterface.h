774f63e9141a93efb50bde44f300b2ca0d2dfe8a
Nora-khairy <khairyn714@gmail.com>
2026-09-15 11:00:21 -0700

    Add DataSourceInterface for client management

---
+++ DataSourceInterface.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include "Client.h"
+#include "Employee.h"
+#include "Admin.h"
+
+using namespace std;
+
+class DataSourceInterface {
+public:
+    virtual void addClient(Client client) = 0;
+    virtual void addEmployee(Employee employee) = 0;
+    virtual void addAdmin(Admin admin) = 0;
+
+    virtual void getAllClients() = 0;
+    virtual void getAllEmployees() = 0;
+    virtual void getAllAdmins() = 0;
+
+    virtual void removeAllClients() = 0;
+    virtual void removeAllEmployees() = 0;
+    virtual void removeAllAdmins() = 0;
+};
