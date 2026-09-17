d794bdb58f258e4c49893d79e084190afbf7d77e
marwa123p <150007061+marwa123p@users.noreply.github.com>
2026-09-16 18:05:15 -0700

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -6,13 +6,52 @@
 #include "Validation.h"
 #include "Person.h"
 #include "Employee.h"
+#include <vector>
 
 class Admin :public Employee {
+class Admin :public Employee {
+protected:
+	vector<Employee> employees;
+
 public:
 	Admin(int Id, string Name, string Pasword, double salary) :
 		Employee(Id, Name, Pasword, salary) {
 	}
 
+	//add employee
+	void AddEmployee(Employee employee) {
+		employees.push_back(employee);
+	}
+	//search employee
+	Employee* searchEmployee(int id) {
+		for (Employee& employee : employees) {
+			if (employee.getid() == id) {
+				return &employee;
+			}
+		}
+		return nullptr;
+	}
+	//list of employees
+	void* ListEmployees() {
+		cout << "List of Employees:" << endl;
+		for (Employee& employee : employees) {
+			employee.display();
+			cout << "------------------------" << endl;
+		}
+	}
+	//edit employee
+	void EditEmployee(int id, string newName, string newPassword, double newSalary) {
+		Employee* employee = searchEmployee(id);
+		if (employee != nullptr) {
+			employee->setname(newName);
+			employee->setpassword(newPassword);
+			employee->setSalary(newSalary);
+			cout << "Employee information updated successfully." << endl;
+		}
+		else {
+			cout << "Employee not found." << endl;
+		}
+	}
 	void display() {
 		Person::displayPerson();
 		cout << salary;
